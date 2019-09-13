c361c17146860505d8bae1171103bd45a81e87e0
tlaudahl <travis.laudahl@gmail.com>
2019-09-13 13:00:39 -0500

    MVP DONE

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import InfoCard from './components/InfoCard';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState('https://swapi.co/api/people/?page=1');
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
+    axios.get(`${page}`)
+    .then(response => {
+      console.log(response.data);
+      setData(response.data.results);
+      setPage(response.data.next);
+    }).catch(error => {
+      console.log('The data could not be returned', error);
+    });
+  }, []);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -12,6 +26,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+        <div className='CardContainer'>
+          {data.map(item => {
+            return <InfoCard name={item.name} birthYear={item.birth_year} height={item.height} mass={item.mass}key={item.name} />
+          })}
+        </div>
     </div>
   );
 }
